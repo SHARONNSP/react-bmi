d26dda7af2a3c38fb0403945c5638014dae83b61
SHARONNSP <31313023+SHARONNSP@users.noreply.github.com>
2021-08-16 20:27:46 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,91 @@
+
+import React, {useState} from 'react';
+import { ImageBackground, StyleSheet, Text, TextInput, TouchableHighlight, View, Keyboard } from 'react-native';
+
+export default function App() {
+  const [height, setHeight] = useState(0)
+  const [weight, setWeight] = useState(0)
+  const [bmi, setBmi] = useState(0)
+  const [indicator, setIndicator] = useState('')
+  const calculateBmi = () => {
+    Keyboard.dismiss()
+    let bmic = weight / ((height/100)**2)
+    setBmi(bmic.toFixed(1))
+    if(bmic < 18){
+      setIndicator("Underweight")
+    }
+    else if(bmic < 25){
+      setIndicator("Normal")
+    }
+    else if(bmic < 25){
+      setIndicator("Overweight")
+    }
+    else{
+      setIndicator("Obese")
+    }
+  }
+
+  return (
+    <TouchableHighlight onPress={Keyboard.dismiss}>
+    <ImageBackground source={require('./assets/bg.jpg')} style={{width:'100%', height:'100%'}}>
+    <View style={styles.container}>
+      <Text style={styles.headingText}>BMI Calculator</Text>
+
+
+    {/* Almost similar to div class row*/ }
+    <View style={{flexDirection: 'row'}}>
+      <TextInput 
+      placeholder="Height" 
+      keyboardType="numeric" 
+      style={styles.TextInputStyle}
+      value={height}
+      onChangeText={text=>setHeight(text)}
+      />
+      <TextInput 
+      placeholder="Weight" 
+      keyboardType="numeric" 
+      style={styles.TextInputStyle}
+      value={weight}
+      onChangeText={text=>setWeight(text)}
+      />
+      </View>
+    <TouchableHighlight onPress={calculateBmi}>
+      <Text style={{textAlign:'center',fontSize:30, color:'orange', padding:30}}>Calculate</Text>
+    </TouchableHighlight>
+    <Text style={styles.textStyle}>{bmi}</Text>
+    <Text style={styles.textStyle}>{indicator}</Text>
+    </View>
+    </ImageBackground>
+    </TouchableHighlight>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  
+  },
+
+  headingText: {
+    color: 'yellow',
+    marginTop:30,
+    textAlign:'center',
+    fontSize:30
+  },
+
+  TextInputStyle: {
+    height:80,
+    textAlign:"center",
+    width:'50%',
+    fontSize:50,
+    marginTop:24,
+    color:'yellow'
+  },
+
+  textStyle: {
+    textAlign:'center',
+    fontSize:30, 
+    color:'yellow', 
+    padding:30
+  }
+});
